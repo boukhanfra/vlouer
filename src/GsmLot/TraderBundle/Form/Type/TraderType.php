1c39758b8cc729ea25dd40983c88a0707bf7c9c8
nizar <nizar.ibaline@gmail.com>
2015-11-24 23:30:14 +0000

    responive design and ajaxed and associated country cities of traderType

--- src/GsmLot/TraderBundle/Form/Type/TraderType.php
+++ src/GsmLot/TraderBundle/Form/Type/TraderType.php
@@ -5,9 +5,25 @@ namespace GsmLot\TraderBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use GsmLot\TraderBundle\Entity\Country;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormInterface;
 
 class TraderType extends AbstractType
 {
+	
+	private $countryId;
+	
+	
+	
+	public function __construct($countryId)
+	{
+		$this->countryId = $countryId;
+	}
+	
+	
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
@@ -25,9 +41,31 @@ class TraderType extends AbstractType
             ->add('faxNumber','text')
 		    ->add('jobTitle','entity',array('class'=>'GsmLot\TraderBundle\Entity\JobTitle'))
 		    ->add('country','entity',array('class'=>'GsmLot\TraderBundle\Entity\Country'))
-		    ->add('city','entity',array('class'=>'GsmLot\TraderBundle\Entity\City'))
-		       
+		    ->setMethod('get')       
         ;
+        
+		    $countryId=$this->countryId;
+		    
+		    $formUpdate  = function(FormInterface $form) use ( $countryId ) 
+		    {
+		    	$options =array(
+		    			'class' => 'GsmLot\TraderBundle\Entity\City',
+		    			'query_builder' => function(EntityRepository $er ) use ( $countryId ) {
+		    			return $er->createQueryBuilder('w')
+		    			->orderBy('w.name', 'ASC')
+		    			->where('w.country = :country_id')
+		    			->setParameter('country_id', $countryId);
+		    			});
+		    	
+		    	$form->add('city','entity',$options);
+		    	
+		    };
+		    
+		 $builder->addEventListener(FormEvents::PRE_SET_DATA,function(FormEvent $event)use($formUpdate)
+		 {
+				$formUpdate($event->getForm());
+		   });
+    
     }
     
     /**
@@ -36,13 +74,21 @@ class TraderType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
+          
             'data_class' => 'GsmLot\TraderBundle\Entity\Trader'
+        	
         ));
+        
+    
     }
+
+    
 
     /**
      * @return string
      */
+    
+    
     public function getName()
     {
         return 'gsmlot_traderbundle_trader';
