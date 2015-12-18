b020f6df0224527fdf9843f1310ea4cb7ad64597
Aziz BOUKHANFRA <a.boukhanfra@gmail.com>
2015-12-18 23:06:40 +0100

    Mise en place de la partie de gestion des traders

--- src/GsmLot/TraderBundle/Controller/TraderController.php
+++ src/GsmLot/TraderBundle/Controller/TraderController.php
@@ -2,18 +2,95 @@
 
 namespace GsmLot\TraderBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
+
 
 class TraderController extends Controller
 {
+
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @var BreadCrumbs
      */
-    public function indexAction($name)
+    private $breadcrumbs;
+
+    /**
+     * @param ContainerInterface $container
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container = null)
     {
-        return array('name' => $name);
+        $this->container = $container;
+
+        $this->breadcrumbs = $this->get("white_october_breadcrumbs");
+
+        $this->breadcrumbs->addItem('index.menu.home',$this->get('router')->generate('index_index'));
+        $this->breadcrumbs->addItem('index.menu.traders',$this->get('router')->generate('trader_list'));
+
+    }
+
+    /**
+     * @Route("/",name="trader_list")
+     * @Security("has_role('ROLE_ADMIN')")
+     * @return Response
+     * @param $request Request
+     */
+    public function indexAction(Request $request)
+    {
+        $trader_list = $this->get('gsm_lot_trader.trader_manager')->getTraders();
+
+        $paginator = $this->get('knp_paginator');
+
+        $pagination = $paginator->paginate($trader_list,$request->query->getInt('page',1),10);
+
+        return $this->render('GsmLotTraderBundle:Trader:list.html.twig',array('pagination'=>$pagination));
+    }
+
+
+    /**
+     * @param Request $request
+     * @Route("/enable/{trader_id}",name="trader_enable")
+     * @return Response
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function enableAction(Request $request)
+    {
+        $trader = $this->get('gsm_lot_trader.trader_manager')->getTrader($request->get('trader_id'));
+
+        if($trader)
+        {
+            $this->get('gsm_lot_trader.trader_manager')->enableTrader($trader);
+
+            $this->get('session')->getFlashBag()->add('notice','trader.enable_success');
+        }
+
+        return $this->redirect($this->get('router')->generate('trader_list'));
+    }
+
+
+
+    /**
+     * @param Request $request
+     * @Route("/disable/{trader_id}",name="trader_disable")
+     * @return Response
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function disableAction(Request $request)
+    {
+        $trader = $this->get('gsm_lot_trader.trader_manager')->getTrader($request->get('trader_id'));
+
+        if($trader)
+        {
+            $this->get('gsm_lot_trader.trader_manager')->disableTrader($trader);
+
+            $this->get('session')->getFlashBag()->add('notice','trader.disable_success');
+        }
+
+        return $this->redirect($this->get('router')->generate('trader_list'));
     }
 }
